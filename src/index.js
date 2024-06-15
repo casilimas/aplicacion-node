d79a8661878b224dae139c38468ae8141a4186ba
freddy andres casilimas sanchez <tu_correo@example.com>
2024-06-15 10:05:59 -0400

    Actualización dependencias cambios en el cors anexo de helmet

--- src/index.js
+++ src/index.js
@@ -2,13 +2,30 @@ const express = require('express');
 const connectDB = require('./db/connection');
 require('dotenv').config();
 const cors = require('cors');
-
+const helmet = require('helmet');  //https://www.npmjs.com/package/helmet#cross-origin-resource-policy
+                                  //middleware de Node.js que mejora la seguridad HTTP configurando varios 
+                                  //encabezados de seguridad para proteger aplicaciones Express.
 const rutas = require('./routers/rutas'); 
 const app = express();
-app.use(cors({
-    origin: 'http://localhost:5173' 
-  }));
 
+                                                                //Configuración de CORS con origen dinámico, 
+// Lista de orígenes permitidos dependiendo del entorno           permite orígenes específicos según el entorno 
+const permitirOrigenes = process.env.NODE_ENV === 'production'  //(localhost para desarrollo y producción), 
+  ? ['https://frontend-produccion.com']                         //utilizando Helmet para seguridad HTTP y Express 
+  : ['http://localhost:5173']; //desarrollo                       para manejar rutas y conexión a la base de datos.
+                                                                //https://www.npmjs.com/package/cors
+  app.use(cors({                                                
+    origin: function (origin, callback) {
+      if (!origin || permitirOrigenes.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('No permitida por CORS'));
+      }
+    }
+  }));;
+
+// Usar Helmet para seguridad HTTP
+app.use(helmet());
 // Conectar a la base de datos
 connectDB();
 
