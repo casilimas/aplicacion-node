cfd340712697510f84fe010553125c11c7134989
freddy andres casilimas sanchez <tu_correo@example.com>
2024-06-16 12:47:09 -0400

    Actualizar connection.js para usar DB_URI

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,3 +1,5 @@
+
+
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
@@ -8,12 +10,20 @@ const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 const dbHost = process.env.DB_HOST; 
 const dbPort = process.env.DB_PORT; 
-const dbName = process.env.DB_NAME; 
-const mongoUri = `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`;
+const dbName = process.env.DB_NAME;
+
+// Construye la cadena de conexión para la base de datos local
+const localUri = `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`;
+
+// Usa la cadena de conexión del clúster si está definida, de lo contrario, usa la local
+const mongoUri = process.env.DB_URI || localUri;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoUri);
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('Conexión a la base de datos exitosamente');
   } catch (error) {
     console.error('Error conectando a la base de datos:', error);
