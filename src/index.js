0a31afad95f0ddcd86c8e791bcb64f4a3cc56839
freddy andres casilimas sanchez <tu_correo@example.com>
2024-06-19 16:09:52 -0400

    Actualizar configuración de reCAPTCHA

--- src/index.js
+++ src/index.js
@@ -2,38 +2,65 @@ const express = require('express');
 const connectDB = require('./db/connection');
 require('dotenv').config();
 const cors = require('cors');
-const helmet = require('helmet');  //https://www.npmjs.com/package/helmet#cross-origin-resource-policy
-                                  //middleware de Node.js que mejora la seguridad HTTP configurando varios 
-                                  //encabezados de seguridad para proteger aplicaciones Express.
-const rutas = require('./routers/rutas'); 
+const helmet = require('helmet');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const rutas = require('./routers/rutas');
+
 const app = express();
 
-                                                                //Configuración de CORS con origen dinámico, 
-// Lista de orígenes permitidos dependiendo del entorno           permite orígenes específicos según el entorno 
-const permitirOrigenes = process.env.NODE_ENV === 'production'  //(localhost para desarrollo y producción), 
-  ? ['https://frontend-produccion.com']                         //utilizando Helmet para seguridad HTTP y Express 
-  : ['http://localhost:5173']; //desarrollo                       para manejar rutas y conexión a la base de datos.
-                                                                //https://www.npmjs.com/package/cors
-  app.use(cors({                                                
-    origin: function (origin, callback) {
-      if (!origin || permitirOrigenes.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('No permitida por CORS'));
-      }
+// Lista de orígenes permitidos dependiendo del entorno
+const permitirOrigenes = process.env.NODE_ENV === 'production'
+  ? ['https://casilimas.github.io']
+  : ['http://localhost:5173', 'http://192.168.31.166:5173'];
+
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('Orígenes permitidos:', permitirOrigenes);
+
+app.use(cors({
+  origin: function (origin, callback) {
+    console.log('Solicitando desde origen:', origin);
+    if (!origin || permitirOrigenes.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      console.error('No permitida por CORS:', origin);
+      callback(new Error('No permitida por CORS'));
     }
-  }));;
+  }
+}));
 
 // Usar Helmet para seguridad HTTP
 app.use(helmet());
+
 // Conectar a la base de datos
 connectDB();
 
 // Middlewares y rutas
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use('/api', rutas);
 app.use('/aplicacion', rutas);
 
+// Ruta para crear usuario y verificar reCAPTCHA
+app.post('/api/crear', async (req, res) => {
+  const recaptchaResponse = req.body.recaptchaToken;
+  const secretKey = process.env.RECAPTCHA_SECRET_KEY;
+
+  const verificationURL = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${recaptchaResponse}`;
+
+  try {
+    const response = await axios.post(verificationURL);
+    if (response.data.success) {
+      res.json({ success: true, message: 'reCAPTCHA verificado con éxito' });
+    } else {
+      res.json({ success: false, message: 'Falló la verificación de reCAPTCHA' });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Error al verificar reCAPTCHA' });
+  }
+});
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto  ${PORT}`));
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
